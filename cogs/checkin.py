4d2de20edd82765be3ce36cc97656dc83bf4a005
zachphi11 <zacharyhphi@gmail.com>
2026-06-03 22:47:14 -0500

    code block bug

--- cogs/checkin.py
+++ cogs/checkin.py
@@ -63,7 +63,7 @@ class CheckinCog(commands.Cog):
             "",
             f"**{username}** checked in: ",
             "```",
-            "{workout}",
+            f"{workout}",
             f"+{XP_PER_CHECKIN} XP  🏋️ |  +{gold_earned}g 💰",
             f"Streak: {new_streak} day{'s' if new_streak != 1 else ''}  |  Level {new_level}  |  Total XP: {new_xp}",
             "```",
